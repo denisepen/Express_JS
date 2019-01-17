2a7cdb1a3714bdc186ead81044e66d59eb905e29
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-01-16 22:01:22 -0500

    passing data to template with ejs

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send("You're on the home page!")
+  res.render('home.ejs')
+  // res.send("You're on the home page!")
+})
+
+app.get('/review/:thing', function(req,res){
+  var thing = req.params.thing;
+  res.render('review.ejs', {thingVar: thing})
 })
 
 app.listen(3000, function(){
